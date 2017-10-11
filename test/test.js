a56bcfd470ac7ed95d807f44d4709ad693d41643
IntroG <gauravmaheshwari57@gmail.com>
2017-10-11 23:56:09 +0530

    add support for chai and mocha test framework.

---
+++ test/test.js
@@ -0,0 +1,18 @@
+var should = require('chai').should() //actually call the function
+;
+
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('foo test', function() {
+        var foo = 'bar'
+        , beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+        
+        foo.should.be.a('string');
+        foo.should.equal('bar');
+        foo.should.have.lengthOf(3);
+        beverages.should.have.property('tea').with.lengthOf(3);
+    });
+  });
+});
+
